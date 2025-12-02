cebeca8a7df7cab520e2b45dfaee3d3355ab99e0
Marc Hoeltge <hoeltgemarc@gmail.com>
2025-12-01 19:09:21 -0500

    fix day1 part 2

--- 2025/day01.py
+++ 2025/day01.py
@@ -5,26 +5,17 @@ def solve(lines):
     curr = 50    
     for line in lines:
         direction, degree = line[0], int(line[1:])
-        movement = degree if direction == 'R' else -degree
-        prev = curr
-        curr = curr + movement
-        clicks_past_zero, curr = divmod(curr, 100)
-        if curr == 0:
-            p1 += 1
-            if direction == 'L':
+        for _ in range(degree): #lol
+            curr += 1 if direction == 'R' else -1
+            curr %= 100
+            if curr == 0:
                 p2 += 1
 
-        p2 += abs(clicks_past_zero)
-        
-        if prev == 0 and clicks_past_zero:
-            p2 -= 1
-        
-    
-    if curr == 0:
-        p2 += 1
+        if curr == 0:
+            p1 += 1
 
     print('part 1:', p1)
-    print('part 2:', p2) # something wrong lol bsearched it
+    print('part 2:', p2)
 
 example_input = open('example_input.txt', 'r').readlines()
 actual_input = open('input.txt', 'r').readlines()
